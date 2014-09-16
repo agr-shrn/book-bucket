d9a65eb85e51a462a1e70b9b9b24dc6ce6fb7d40
Sharan Agrawal <sharan.jmi@gmail.com>
2014-09-16 19:36:51 +0530

    Update collect.py

--- collect.py
+++ collect.py
@@ -1,45 +1,43 @@
 import os
 import json
+
 from facepy import GraphAPI
-
-access_token = ""
-
+access_token = "CAACEdEose0cBAMCjAYqfPB4ZB9BSxpcRVPKfdgOnr1pE2nVkQn6ZBoTNUt7uz2cEQho46OZABekn95ur8Ioew9DPUmTdUgd21XBiW6fcsLTs7sp6lmNJeGZAQlpZCpQCVUjXnvBcH19znSXyO8mQWN8synWCwadfqGQesZCYXkSBj2sibD7ApD1Y58r3egPyL8zOnBTwW18CV7uZAZCDm8c9"
 graph = GraphAPI(access_token)
 
+folder={};
 # Create the output folder if it doesn't already exist
 folder['posts'] = 'data/posts'
 folder['friends'] = 'data/friends'
 
 if not os.path.isdir(folder['posts']):
-    os.mkdir(folder['posts'])
-
+	os.mkdir(folder['posts'])
 if not os.path.isdir(folder['friends']):
-    os.mkdir(folder['friends'])
-
+	os.mkdir(folder['friends'])
 try:
-    os.remove('data/posts/posts.json')
-    os.remove('data/friends/friends.json')
+	os.remove('data/posts/posts.json')
+	os.remove('data/friends/friends.json')
 except:
-    pass
-
+	pass
+os.system('cd data/friends')
 # Gather friends
+
 friend_pages = graph.get("me/friends", page=True)
-with open('friends.json', 'a') as f:
-    count = 0
-    for friend_page in friend_pages:
-        json.dump(friend_page, f, indent=1)
-
-        count = count + 1
-        if count >= 5:
-            break
-
+i=1
+for friend_page in friend_pages:
+	fri_file = "data/friends/" + str(i) + ".json"
+	fh = open(fri_file,'a')
+	json.dump(friend_page, fh, indent=1)
+	i=i+1
+	if(i>=5):
+		break
 # Gather posts
 posts_pages = graph.get('me/posts', page=True)
 with open('posts.json', 'a') as f:
-    count = 0
-    for posts_page in posts_pages:
-        json.dump(posts_page, f, indent=1)
-
-        count = count + 1
-        if count >= 5:
-            break
+	count = 0
+	for posts_page in posts_pages:
+		json.dump(posts_page, f, indent=1)
+		
+		count = count + 1
+		if count >= 5:
+			break
